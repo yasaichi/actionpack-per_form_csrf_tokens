18c201cc52a81443049287e71aaa7d59564f95d4
yasaichi <yasaichi@users.noreply.github.com>
2021-09-08 10:58:10 +0900

    Initial Commit

---
+++ lib/actionpack/per_form_csrf_tokens.rb
@@ -0,0 +1,8 @@
+require "actionpack/per_form_csrf_tokens/version"
+
+module Actionpack
+  module PerFormCsrfTokens
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/actionpack/per_form_csrf_tokens/version.rb
@@ -0,0 +1,5 @@
+module Actionpack
+  module PerFormCsrfTokens
+    VERSION = "0.1.0"
+  end
+end
