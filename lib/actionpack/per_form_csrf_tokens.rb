a2f4328d79c018c2cbf240097075f5a6d9fd2a47
yasaichi <yasaichi@users.noreply.github.com>
2021-09-08 20:07:22 +0900

    Implement the core feature

--- lib/actionpack/per_form_csrf_tokens.rb
+++ lib/actionpack/per_form_csrf_tokens.rb
@@ -1,1 +1,8 @@
+require "active_support"
+require "active_support/lazy_load_hooks"
+require "actionpack/per_form_csrf_tokens/request_forgery_protection"
 require "actionpack/per_form_csrf_tokens/version"
+
+ActiveSupport.on_load(:action_controller) do
+  include(ActionPack::PerFormCsrfTokens::RequestForgeryProtection)
+end
